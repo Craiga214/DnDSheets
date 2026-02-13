b9415e87cd36dbed8f9612404fa1254a5cbbbdd9
CraigArchibald <carchibald@riis.com>
2026-02-13 18:12:21 -0500

    Pages fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: '/DnDSheets/', // <-- exact repo name with correct capitalization
+  plugins: [react()],
+  base: '/DnDSheets/', // EXACT repo name, case-sensitive
 })
